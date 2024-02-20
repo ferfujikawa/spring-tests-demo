d9e3fbcd642f22ff9644be6a8625381661ac387f
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-20 14:01:28 -0300

    Criação de teste do método sortearMedicosAtivosDeUmaEspecialidade quando há pelo menos um médico com a especialidade em questão.

--- src/test/java/com/fujikawa/springtestsdemo/repositories/MedicoRepositoryTest.java
+++ src/test/java/com/fujikawa/springtestsdemo/repositories/MedicoRepositoryTest.java
@@ -30,6 +30,11 @@ public class MedicoRepositoryTest {
         this.entityManager = entityManager;
     }
 
+    private Medico cadastrarMedico(String nome, String email, String crm, String especialidade) {
+        
+        return entityManager.persist(new Medico(nome, email, especialidade, crm));
+    }
+
     @Test
     @DisplayName("Deveria devolver uma lista vazia quando não há médicos com determinada especialidade")
     void testSortearMedicoAtivoDeUmaEspecialidadeCenario1() {
@@ -44,8 +49,17 @@ public class MedicoRepositoryTest {
         Assertions.assertThat(medicosDaEspecialidade).isEmpty();
     }
 
-    private Medico cadastrarMedico(String nome, String email, String crm, String especialidade) {
-        
-        return entityManager.persist(new Medico(nome, email, especialidade, crm));
+    @Test
+    @DisplayName("Deveria devolver uma lista de médicos quando há médicos com determinada especialidade")
+    void testSortearMedicoAtivoDeUmaEspecialidadeCenario2() {
+
+        cadastrarMedico("Médico teste 1", "medicoteste1@email.com", "121212", "ORTOPEDIA");
+        cadastrarMedico("Médico teste 2", "medicoteste2@email.com", "232323", "CARDIOLOGIA");
+        cadastrarMedico("Médico teste 3", "medicoteste3@email.com", "343434", "INFECTOLOGIA");
+        cadastrarMedico("Médico teste 4", "medicoteste4@email.com", "454545", "ORTOPEDIA");
+        cadastrarMedico("Médico teste 5", "medicoteste5@email.com", "565656", "CARDIOLOGIA");
+
+        List<Medico> medicosDaEspecialidade = medicoRepository.sortearMedicosAtivosDeUmaEspecialidade("INFECTOLOGIA");
+        Assertions.assertThat(medicosDaEspecialidade).isNotEmpty();
     }
 }
