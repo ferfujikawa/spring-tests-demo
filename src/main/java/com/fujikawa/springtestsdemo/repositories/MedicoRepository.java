3b97d1e0f0f7703960bee47ceaff40bf77643365
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-20 09:18:49 -0300

    Inclusão da annotation @Repository em MedicoRepository.

--- src/main/java/com/fujikawa/springtestsdemo/repositories/MedicoRepository.java
+++ src/main/java/com/fujikawa/springtestsdemo/repositories/MedicoRepository.java
@@ -5,9 +5,11 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.fujikawa.springtestsdemo.entities.Medico;
 
+@Repository
 public interface MedicoRepository extends JpaRepository<Medico, UUID> {
 
     @Query("select m from Medico m where m.ativo = 1 and m.especialidade = :especialidade order by rand() limit 1")
