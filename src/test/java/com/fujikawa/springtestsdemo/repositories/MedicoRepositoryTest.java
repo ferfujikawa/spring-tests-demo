74b20db7bdc0130e1b6d938c37f2aaec564d595c
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-20 13:55:19 -0300

    Utilização de TestEntityManager para inserção de registros de teste.

--- src/test/java/com/fujikawa/springtestsdemo/repositories/MedicoRepositoryTest.java
+++ src/test/java/com/fujikawa/springtestsdemo/repositories/MedicoRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.fujikawa.springtestsdemo.entities.Medico;
@@ -18,17 +19,33 @@ import com.fujikawa.springtestsdemo.entities.Medico;
 public class MedicoRepositoryTest {
 
     private MedicoRepository medicoRepository;
+    private TestEntityManager entityManager;
 
     @Autowired
-    public MedicoRepositoryTest(MedicoRepository medicoRepository) {
+    public MedicoRepositoryTest(
+        MedicoRepository medicoRepository,
+        TestEntityManager entityManager) {
+
         this.medicoRepository = medicoRepository;
+        this.entityManager = entityManager;
     }
 
     @Test
     @DisplayName("Deveria devolver uma lista vazia quando não há médicos com determinada especialidade")
-    void testSortearMedicoAtivoDeUmaEspecialidade() {
+    void testSortearMedicoAtivoDeUmaEspecialidadeCenario1() {
 
-        List<Medico> medicosDaEspecialidade = medicoRepository.sortearMedicosAtivosDeUmaEspecialidade("INEXISTENTE");
+        cadastrarMedico("Médico teste 1", "medicoteste1@email.com", "121212", "ORTOPEDIA");
+        cadastrarMedico("Médico teste 2", "medicoteste2@email.com", "232323", "CARDIOLOGIA");
+        cadastrarMedico("Médico teste 3", "medicoteste3@email.com", "343434", "INFECTOLOGIA");
+        cadastrarMedico("Médico teste 4", "medicoteste4@email.com", "454545", "ORTOPEDIA");
+        cadastrarMedico("Médico teste 5", "medicoteste5@email.com", "565656", "CARDIOLOGIA");
+
+        List<Medico> medicosDaEspecialidade = medicoRepository.sortearMedicosAtivosDeUmaEspecialidade("PSIQUIATRIA");
         Assertions.assertThat(medicosDaEspecialidade).isEmpty();
+    }
+
+    private Medico cadastrarMedico(String nome, String email, String crm, String especialidade) {
+        
+        return entityManager.persist(new Medico(nome, email, especialidade, crm));
     }
 }
