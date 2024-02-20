32a8fe47f06d08fdb23d20a8b2b4564a50e73f73
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-20 12:58:13 -0300

    Correção da query do método para sortear médico de uma especialidade e alterado o mesmo para retornar uma lista de médicos (PostgreSQL não permite a utilização de limit com order by random()).

--- src/main/java/com/fujikawa/springtestsdemo/repositories/MedicoRepository.java
+++ src/main/java/com/fujikawa/springtestsdemo/repositories/MedicoRepository.java
@@ -1,6 +1,6 @@
 package com.fujikawa.springtestsdemo.repositories;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,6 +12,6 @@ import com.fujikawa.springtestsdemo.entities.Medico;
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, UUID> {
 
-    @Query("select m from Medico m where m.ativo = 1 and m.especialidade = :especialidade order by rand() limit 1")
-    Optional<Medico> sortearMedicoAtivoDeUmaEspecialidade(String especialidade);
+    @Query("select m from Medico m where m.ativo = 1 and m.especialidade = :especialidade order by RANDOM()")
+    List<Medico> sortearMedicosAtivosDeUmaEspecialidade(String especialidade);
 }
