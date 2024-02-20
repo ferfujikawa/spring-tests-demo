9e6d08a4893faa1d70a8d8126739148279211235
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-20 14:29:52 -0300

    Criação dos DTOs de médico e da ação de cadastrar médico.

---
+++ src/main/java/com/fujikawa/springtestsdemo/dtos/MedicoDTO.java
@@ -0,0 +1,77 @@
+package com.fujikawa.springtestsdemo.dtos;
+
+import java.util.UUID;
+
+public class MedicoDTO {
+
+    private UUID id;
+
+    private String nome;
+
+	private String email;
+
+    private Boolean ativo;
+
+	private String crm;
+	
+	private String especialidade;
+
+    protected MedicoDTO() {}
+
+    public MedicoDTO(UUID id, String nome, String email, Boolean ativo, String crm, String especialidade) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.ativo = ativo;
+        this.crm = crm;
+        this.especialidade = especialidade;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+}
