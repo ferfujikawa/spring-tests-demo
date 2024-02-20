b2cb98c6b5a033ade6ab1669d1a76267c72e12d6
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-20 15:23:43 -0300

    Criação da classe de teste do controller de médico com método de teste para execução com sucesso da requisição POST em /medicos.

---
+++ src/test/java/com/fujikawa/springtestsdemo/controllers/MedicoControllerTest.java
@@ -0,0 +1,89 @@
+package com.fujikawa.springtestsdemo.controllers;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import com.fujikawa.springtestsdemo.dtos.CadastrarMedicoDTO;
+import com.fujikawa.springtestsdemo.dtos.MedicoDTO;
+import com.fujikawa.springtestsdemo.services.CadastrarMedicoService;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+public class MedicoControllerTest {
+    
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<CadastrarMedicoDTO> dadosCadastroMedicoMock;
+
+    @Autowired
+    private JacksonTester<MedicoDTO> dadosMedicoCadastradoMock;
+
+    @MockBean
+    private CadastrarMedicoService cadastrarMedicoService;
+
+    @Test
+    @DisplayName("Deveria devolver código http 204 quando informações válidas")
+    void testCadastrarMedicoCenario1() throws IOException, Exception {
+
+        UUID novoMedicoId = UUID.randomUUID();
+
+        CadastrarMedicoDTO dadosCadastroMedico = new CadastrarMedicoDTO(
+            "Novo medico",
+            "novo.medico@email.com",
+            "CARDIOLOGIA",
+            "123555");
+
+        MedicoDTO dadosMedicoCadastrado = new MedicoDTO(
+            novoMedicoId,
+            dadosCadastroMedico.getNome(),
+            dadosCadastroMedico.getEmail(),
+            true,
+            dadosCadastroMedico.getCrm(),
+            dadosCadastroMedico.getEspecialidade());
+
+        when(cadastrarMedicoService.executar(any())).thenReturn(dadosMedicoCadastrado);
+
+        MockHttpServletResponse response = mvc
+            .perform(
+                post("/medicos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(dadosCadastroMedicoMock.write(dadosCadastroMedico).getJson()))
+            .andReturn()
+            .getResponse();
+
+        MedicoDTO dadosMedicoCadastradoEsperado = new MedicoDTO(
+            novoMedicoId,
+            dadosCadastroMedico.getNome(),
+            dadosCadastroMedico.getEmail(),
+            true,
+            dadosCadastroMedico.getCrm(),
+            dadosCadastroMedico.getEspecialidade());
+
+        String jsonEsperado = dadosMedicoCadastradoMock.write(dadosMedicoCadastradoEsperado).getJson();
+
+        Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+        Assertions.assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
+    }
+}
