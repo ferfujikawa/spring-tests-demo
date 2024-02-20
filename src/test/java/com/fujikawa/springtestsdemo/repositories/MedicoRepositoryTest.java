8078519fa8c9bac8d3e376f9f42e45d5bcf0fd9b
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-20 13:41:39 -0300

    Implementação básica do teste do método sortearMedicosAtivosDeUmaEspecialidade quando não há médicos com a especialidade em questão.

--- src/test/java/com/fujikawa/springtestsdemo/repositories/MedicoRepositoryTest.java
+++ src/test/java/com/fujikawa/springtestsdemo/repositories/MedicoRepositoryTest.java
@@ -1,17 +1,34 @@
 package com.fujikawa.springtestsdemo.repositories;
 
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import com.fujikawa.springtestsdemo.entities.Medico;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 public class MedicoRepositoryTest {
 
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    public MedicoRepositoryTest(MedicoRepository medicoRepository) {
+        this.medicoRepository = medicoRepository;
+    }
+
     @Test
+    @DisplayName("Deveria devolver uma lista vazia quando não há médicos com determinada especialidade")
     void testSortearMedicoAtivoDeUmaEspecialidade() {
 
+        List<Medico> medicosDaEspecialidade = medicoRepository.sortearMedicosAtivosDeUmaEspecialidade("INEXISTENTE");
+        Assertions.assertThat(medicosDaEspecialidade).isEmpty();
     }
 }
