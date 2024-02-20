89bed4b73993da991600475d41d5410ad3576738
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-20 08:36:12 -0300

    Criação do projeto

---
+++ src/main/java/com/fujikawa/springtestsdemo/SpringTestsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fujikawa.springtestsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fujikawa/springtestsdemo/SpringTestsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fujikawa.springtestsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
